e8dd2280a711afbcfce85b56a0f54daf763500fe
decadent-cuB3r <cub3r.ezra.yu@gmail.com>
2022-04-14 04:00:44 +0800

    mapView ios map

--- screen/LocationScreen.js
+++ screen/LocationScreen.js
@@ -3,24 +3,21 @@ import MapView from 'react-native-maps';
 import { Box, Text, Center, View } from "native-base";
 import { StyleSheet } from "react-native";
 
-  
-export default function LocationScreen () {
-    const onRegionChange=(region)=> {
+
+export default function LocationScreen() {
+    const onRegionChange = (region) => {
         this.setState({ region });
-      }
+    }
     return (
-        // Important! Always set the container height explic
-        <View >
-            <MapView
-                initialRegion={{
-                latitude: 37.78825,
-                longitude: -122.4324,
+        <MapView
+            style={{ flex: 1 }}
+            region={{
+                latitude: 42.882004,
+                longitude: 74.582748,
                 latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
-                }}
-                onRegionChange={onRegionChange}
-            />
-            
-        </View>
+                longitudeDelta: 0.0421
+            }}
+            showsUserLocation={true}
+        />
     );
 };
